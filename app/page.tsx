83321c518f1c87f58a142840c8725b1f9755df1c
Devp-sriram <sriramraman100@gmail.com>
2025-03-24 07:52:38 +0530

    home page edited

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
      <div className="min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold white border-gray-500">Welcome to the employee management app</h1>
+      <h1 className="text-4xl font-bold white border-gray-500">Welcome to dynamic dashboard</h1>
       <div className="py-12">
         <Link
           href="/login"
